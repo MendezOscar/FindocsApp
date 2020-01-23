f359b58259477e9ce6fe6c365b6508f6a664b24e
Oscar Cruz <ocruz@caracolknits.com>
2020-01-23 15:38:09 -0600

    Modify environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCfmaafqIuGqLhBt4Kdkucsz6c6-RZuTy8",
+    authDomain: "finddocdb-995fd.firebaseapp.com",
+    databaseURL: "https://finddocdb-995fd.firebaseio.com",
+    projectId: "finddocdb-995fd",
+    storageBucket: "finddocdb-995fd.appspot.com",
+    messagingSenderId: "110140979305",
+    appId: "1:110140979305:web:30475c7c4d3fa86e990af8",
+    measurementId: "G-TEW6MPLYLV"
+  }
 };
